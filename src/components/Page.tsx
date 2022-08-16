a0dc366763a618a9526bf3dd8ad1a67fab4b3487
chisom5 <okoyechisom54@gmail.com>
2022-08-16 10:10:24 +0100

    initial attempt

---
+++ src/components/Page.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Box } from "@mui/material";
+import { useDrop, useDragDropManager } from "react-dnd";
+
+import Grid from "./Grid";
+import Module from "./Module";
+import { GUTTER_SIZE } from "../constants";
+import ModuleInterface from "../types/ModuleInterface";
+
+const Page = () => {
+  const [modules] = React.useState([
+    { id: 1, coord: { x: 1, y: 80, w: 2, h: 200 } },
+    { id: 2, coord: { x: 5, y: 0, w: 3, h: 100 } },
+    { id: 3, coord: { x: 4, y: 310, w: 3, h: 200 } },
+  ]);
+
+  const containerRef = React.useRef<HTMLDivElement>();
+
+  // I added this
+  const dndManager = useDragDropManager();
+  const monitor = dndManager.getMonitor();
+
+  const findCard = React.useCallback(
+    (id: number) => {
+      const module = modules.filter((c) => c.id === id)[0];
+      return {
+        module,
+        index: modules.indexOf(module),
+      };
+    },
+    [modules]
+  );
+// down to here
+
+  // Wire the module to DnD drag system
+  const [, drop] = useDrop({
+    accept: "module",
+  });
+
+  drop(containerRef);
+
+  // Calculate container height
+  const containerHeight = React.useMemo(
+    () =>
+      Math.max(...modules.map(({ coord: { y, h } }) => y + h)) +
+      GUTTER_SIZE * 2,
+    [modules]
+  );
+
+  return (
+    <Box
+      ref={containerRef}
+      position="relative"
+      width={1024}
+      height={containerHeight}
+      margin="auto"
+      sx={{
+        overflow: "hidden",
+        outline: "1px dashed #ccc",
+        transition: "height 0.2s",
+      }}
+    >
+      <Grid height={containerHeight} />
+      {modules.map((module) => (
+        <Module key={module.id} data={module} findCard={findCard} />
+      ))}
+    </Box>
+  );
+};
+
+export default React.memo(Page);
---
+++ src/helpers.ts
@@ -0,0 +1,9 @@
+import { COLUMN_WIDTH, GUTTER_SIZE } from './constants';
+
+export const moduleW2LocalWidth = (moduleW: number) => moduleW * COLUMN_WIDTH - GUTTER_SIZE;
+export const moduleX2LocalX = (moduleX: number) => moduleW2LocalWidth(moduleX) + GUTTER_SIZE * 2;
+export const moduleY2LocalY = (moduleY: number) => moduleY + GUTTER_SIZE;
+
+// export const LocalWidth2moduleW = (moduleW: number) => moduleW / COLUMN_WIDTH + GUTTER_SIZE;
+// export const LocalY2moduleY = (moduleY: number) => moduleY - GUTTER_SIZE;
+// export const LocalX2moduleX = (moduleX: number) => LocalWidth2moduleW(moduleX) - GUTTER_SIZE / 2;
